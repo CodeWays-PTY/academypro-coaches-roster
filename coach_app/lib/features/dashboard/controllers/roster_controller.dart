aae0f008527218f5d646d8f2fe583138fe868733
Jan-Albert Mentz <janmen777@gmail.com>
2026-07-18 08:30:57 +0200

    Fix compiler error: import auth_state.dart to resolve apiClientProvider in roster_controller

--- coach_app/lib/features/dashboard/controllers/roster_controller.dart
+++ coach_app/lib/features/dashboard/controllers/roster_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/network/api_client.dart';
+import '../../auth/presentation/auth_state.dart';
 
 class RosterPlayer {
   final String id;
