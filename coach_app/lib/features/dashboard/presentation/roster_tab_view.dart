b3bf72ae69c2fbd9daad39a26c2259b546fadf70
Jan-Albert Mentz <janmen777@gmail.com>
2026-07-20 12:46:54 +0200

    fix: import flutter services to resolve HapticFeedback compiler error in roster tab

--- coach_app/lib/features/dashboard/presentation/roster_tab_view.dart
+++ coach_app/lib/features/dashboard/presentation/roster_tab_view.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../controllers/roster_controller.dart';
 import '../controllers/dashboard_controller.dart';
