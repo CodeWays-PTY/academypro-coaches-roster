8f1334ae9e183d6b98bef6bb34475be2ebd9071f
Jan-Albert Mentz <janmen777@gmail.com>
2026-07-24 08:49:19 +0200

    Add auth_state import to main.dart for clean sign out resolution

--- academypro_app/lib/main.dart
+++ academypro_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/storage/local_storage.dart';
 import 'core/theme/app_theme.dart';
+import 'features/auth/presentation/auth_state.dart';
 import 'features/auth/presentation/login_screen.dart';
 import 'features/dashboard/presentation/dashboard_screen.dart';
 import 'features/student/presentation/student_dashboard_screen.dart';
