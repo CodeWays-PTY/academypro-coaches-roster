89a56e9a40887b2f999ffe76d8088efb6bcf5f13
Jan-Albert Mentz <janmen777@gmail.com>
2026-08-05 08:59:17 +0200

    Enhance RosterPlayer JSON type safety and purge test records from D1 database

--- academypro_app/lib/features/dashboard/controllers/roster_controller.dart
+++ academypro_app/lib/features/dashboard/controllers/roster_controller.dart
@@ -11,7 +11,7 @@ class SquadInfo {
 
   SquadInfo({required this.id, required this.name, required this.code});
 
-  factory SquadInfo.fromJson(Map<String, dynamic> json) {
+  factory SquadInfo.fromJson(Map<dynamic, dynamic> json) {
     return SquadInfo(
       id: TypeParsers.parseString(json['id']),
       name: TypeParsers.parseString(json['name']),
@@ -43,18 +43,24 @@ class RosterPlayer {
     List<SquadInfo>? assignedSquads,
   })  : assignedSquads = assignedSquads ?? [];
 
-  factory RosterPlayer.fromJson(Map<String, dynamic> json) {
+  factory RosterPlayer.fromJson(Map<dynamic, dynamic> json) {
     final rawSquads = json['assignedSquads'] as List<dynamic>? ?? [];
+    final squadsList = <SquadInfo>[];
+    for (final s in rawSquads) {
+      if (s is Map) {
+        squadsList.add(SquadInfo.fromJson(s));
+      }
+    }
     return RosterPlayer(
       id: TypeParsers.parseString(json['id']),
       firstName: TypeParsers.parseString(json['firstName']),
       lastName: TypeParsers.parseString(json['lastName']),
       ageGroup: TypeParsers.parseString(json['ageGroup']),
-      position: TypeParsers.parseString(json['position']),
+      position: TypeParsers.parseString(json['position'], 'Athlete'),
       team: TypeParsers.parseString(json['team']),
       status: TypeParsers.parseString(json['status']),
       age: TypeParsers.parseNullableInt(json['age']),
-      assignedSquads: rawSquads.map((s) => SquadInfo.fromJson(s as Map<String, dynamic>)).toList(),
+      assignedSquads: squadsList,
     );
   }
 }
