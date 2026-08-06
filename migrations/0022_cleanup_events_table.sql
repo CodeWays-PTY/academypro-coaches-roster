13b06461ef9f898849ec995f09eb37804cc3c5e4
Jan-Albert Mentz <janmen777@gmail.com>
2026-08-06 10:06:39 +0200

    Clean up events table: remove obsolete batch_id and completion_count columns and populate series_id

--- migrations/0022_cleanup_events_table.sql
+++ migrations/0022_cleanup_events_table.sql
@@ -2,5 +2,7 @@ UPDATE events
 SET series_id = 'EVT-1785925048902' 
 WHERE id LIKE 'EVT-1785925048902_%' OR (title = 'U15 Gym Training' AND (series_id IS NULL OR series_id = ''));
 
+DROP INDEX IF EXISTS idx_events_batch;
+
 ALTER TABLE events DROP COLUMN batch_id;
 ALTER TABLE events DROP COLUMN completion_count;
